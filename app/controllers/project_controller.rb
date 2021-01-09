324a152a1caa77dd23552ce67a982c5dac6b7aa5
alfredopl <aalfredoplz@gmail.com>
2021-01-09 00:46:09 -0300

    controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,7 +1,10 @@
 class ProjectController < ApplicationController
+  #http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
   def index
+    @projects = Project.all()
   end
 
   def dashboard
   end
+
 end
